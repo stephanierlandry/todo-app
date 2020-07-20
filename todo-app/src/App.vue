b970251f431b1a21493a8ec71bd75d7a3bf3a181
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-20 14:37:15 -0500

    Todo list being displayed.

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,27 +1,50 @@
 <template>
   <div id="app">
-    <todo-list list-name="My to-dos" />
+    <Todos v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
-  import TodoList from './components/TodoList.vue'
+  import Todos from './components/Todos';
 
   export default {
-    name: 'App',
+    name: 'app',
     components: {
-      TodoList,
+      Todos
     },
-  };
+    data() {
+      return {
+        todos: [
+          {
+            id: 1,
+            title: 'Go workout',
+            completed: false
+          },
+          {
+            id: 2,
+            title: 'Do laundry',
+            completed: false
+          },
+          {
+            id: 3,
+            title: 'Cook food',
+            completed: false
+          },
+          {
+            id: 4,
+            title: 'Clean up room',
+            completed: false
+          },
+          {
+            i: 5,
+            title: 'Finish work',
+            completed: false
+          }
+        ],
+      }
+    },
+  }
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
 </style>
---
+++ todo-app/src/components/Todo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>{{ todo.title }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Todo',
+    props: [
+      "todo"
+    ]
+  }
+</script>
+
+<style scoped>
+</style>
