b04e99dd02d5a0bf01b01364e7116110e07b21fe
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-20 13:55:42 -0500

    TodoList component created and imported to root

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,27 +1,27 @@
 <template>
   <div id="app">
-    <HelloWorld msg="todo"/>
+    <todo-list list-name="My to-dos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import TodoList from './components/TodoList.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'App',
+    components: {
+      TodoList,
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ todo-app/src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="todo">
+    <h1>todo</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  h1{
+  font-size:5em;
+  font-weight: normal;
+  }
+
+</style>
