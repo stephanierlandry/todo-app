65c5d9678f394b15959bed465affd753b65586e5
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-20 20:32:39 -0500

    X icon added to todo items

--- todo-app/src/components/Todo.vue
+++ todo-app/src/components/Todo.vue
@@ -1,25 +1,38 @@
 <template>
   <div v-bind:class="{ 'completed': todo.completed }" class="display">
     <p v-on:click="markComplete">{{ todo.title }}</p>
-    <button @click="$emit('delete-todo', todo.id)" class="x-btn">X</button>
+    <button @click="$emit('delete-todo', todo.id)" class="x-btn"><x-icon size="1.5x" class="custom-class"></x-icon></button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Todo',
-  props: [
-    "todo"
-  ],
-  methods: {
-    markComplete() {
-      this.todo.completed = !this.todo.completed
+  import { XIcon } from 'vue-feather-icons'
+
+  export default {
+    name: 'Todo',
+    props: [
+      "todo"
+    ],
+    components: {
+      XIcon
+    },
+    methods: {
+      markComplete() {
+        this.todo.completed = !this.todo.completed
+      }
     }
   }
-}
 </script>
 
 <style scoped>
+  p{
+    font-size: 1.5em;
+  }
+
+  p:hover{
+    cursor:pointer;
+  }
+
   .completed {
     text-decoration: line-through;
   }
@@ -32,9 +45,13 @@ export default {
   background:none;
   border:none;
   font-weight: bold;
+  color: red;
+  font-size: 1em;
+  margin-left: 1em;
   }
 
   .x-btn:hover{
   cursor:pointer;
+  color: #A50909;
   }
 </style>
