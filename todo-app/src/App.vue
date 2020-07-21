110307e1450b4359e1e65f866da15edda31e9e4e
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-21 11:38:38 -0500

    dummy data removed

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -31,31 +31,7 @@
     data() {
       return {
         todos: [
-          {
-            id: 1,
-            title: 'Go workout',
-            completed: false
-          },
-          {
-            id: 2,
-            title: 'Do laundry',
-            completed: false
-          },
-          {
-            id: 3,
-            title: 'Cook food',
-            completed: false
-          },
-          {
-            id: 4,
-            title: 'Clean up room',
-            completed: false
-          },
-          {
-            i: 5,
-            title: 'Finish work',
-            completed: false
-          }
+    
         ],
       }
     },
